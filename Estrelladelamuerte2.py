ccc7efe1f04b18bc663e3f0d4a547171ed865468
judithsalas <146074456+judithsalas@users.noreply.github.com>
2024-01-18 12:28:15 +0100

    Update Estrelladelamuerte2.py

--- Estrelladelamuerte2.py
+++ Estrelladelamuerte2.py
@@ -1,3 +1,12 @@
+'''
+En EstrellaDeLaMuerte.py, modifica la clase EstrellaDeLaMuerte para que 
+pueda interactuar con sus naves aliadas. Añade métodos para atacar naves aliadas y 
+verificar si pueden ser destruidas. Si la vida de la nave aliada es menor o igual 
+a los puntos de vida de la Estrella de la Muerte, muestra un mensaje por terminal que 
+diga que la Estrella de la Muerte ha destruido la nave aliada y resta los puntos de vida correspondientes. 
+Si la vida es mayor, muestra un mensaje que diga que la Estrella de la Muerte no puede destruir la nave aliada.
+'''
+
 class EstrellaDeLaMuerte:
     def __init__(self, puntos_de_vida):
         self.puntos_de_vida = puntos_de_vida
